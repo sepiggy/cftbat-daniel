656c21600ce2fca21b5f0fcbee54a4e3a342dd72
sepiggy <jms1209@qq.com>
2018-06-01 10:20:42 +0800

    B4 p59 ok

--- src/cftbat_daniel/ch03/functions.clj
+++ src/cftbat_daniel/ch03/functions.clj
@@ -24,6 +24,97 @@
 (defn x-chop
   "Describe the kind of chop you're inflicting on someone"
   ([name chop-type]
-    (str "I " chop-type " chop " name "! Take that!"))
+   (str "I " chop-type " chop " name "! Take that!"))
   ([name]
-    (x-chop name "Karate")))
+   (x-chop name "Karate")))
+
+(x-chop "Kanye West" "slap")
+
+; 不定参
+(defn codger-communication
+  [whippersnapper]
+  (str "Get off my lawn, " whippersnapper "!!!"))
+
+(defn codger
+  [& whippersnappers]
+  (map codger-communication whippersnappers))
+
+(codger "Billy" "Anne-Marie" "The Incredible Bulk")
+
+; 正常参数与不定参在一起, 不定参要写在最后
+(defn favorite-things
+  [name & things]
+  (str "Hi, " name ", here are my favorite things: "
+       (clojure.string/join ", " things)))
+
+(favorite-things "Doreen" "gum" "shoes" "kara-te")
+
+;; Return the first element of a collection
+(defn my-first
+  [[first-thing]]
+  first-thing)
+
+(my-first ["oven" "bike" "war-axe"])
+
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We're ignoring the rest of your choices. "
+                "Here they are in case you need to cry over them: "
+                (clojure.string/join ", " unimportant-choices))))
+
+(chooser ["Marmalade", "Handsome Jack", "Pigpen", "Aquaman"])
+
+(defn announce-treasure-location
+  [{lat :lat lng :lng}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lng: " lng)))
+
+(announce-treasure-location {:lat 28.22 :lng 81.33})
+
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "Joe")
+
+(illustrative-function)
+
+(defn number-comment
+  [x]
+  (if (> x 6)
+    "Oh my gosh! What a big number!"
+    "That number's OK, I guess"))
+(number-comment 5)
+(number-comment 7)
+
+; 4. Anonymous Functions
+; 4.1 通过 fn 创建匿名函数
+(map (fn [name] (str "Hi, " name))
+     ["Darth Vader" "Mr. Magoo"])
+
+((fn [x] (* x 3)) 8)
+
+;; 赋给匿名函数一个名字
+(def my-special-multiplier (fn [x] (* x 3)))
+(my-special-multiplier 12)
+
+; 4.2 通过 # 创建匿名函数
+#(* % 3)
+(#(* % 3) 8)
+
+(map #(str "Hi, " %)
+     ["Darth Vader" "Mr. Magoo"])
+
+(#(str %1 " and " %2) "cornbread" "butter beans")
+
+(#(identity %&) 1 "blarg" :yip)
+
+; 5. Returning Functions
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+(def inc3 (inc-maker 3))
+(inc3 7)
